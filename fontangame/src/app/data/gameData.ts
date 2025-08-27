7b50520196abddb6d501d47f37c516855c76df8a
munrogr84298 <munrogr84298@th-nuernberg.de>
2025-08-27 11:19:56 +0200

    Fixed the JSON Import and added test data creation for testing

--- fontangame/src/app/data/gameData.ts
+++ fontangame/src/app/data/gameData.ts
@@ -1,5 +1,5 @@
-import { bpmList } from './bpmData';
-import { spo2List } from './spo2Data';
+import { bpmList, bpmUnit } from './bpmData';
+import { spo2List, spo2Unit } from './spo2Data';
 
 export class GameData {
 
@@ -55,20 +55,58 @@ export class GameData {
 
     public static CreateFromJson(json: string): GameData {
         const data = JSON.parse(json);
+        console.log(data);
+        //I hate the json implementation in angular...this stuff is necessary so these things work like the proper objects they are
+        const bpmJson = Object.assign(new bpmList(), data.bpmList);
+        const bpmData = new bpmList();
+        bpmJson.GetAll().forEach((element: any) => {
+            element = Object.assign(new bpmUnit(0, 0, false), element);
+            bpmData.Add(element);
+        });
+        //and again for spo2 data
+        const spo2Json = Object.assign(new spo2List(), data.spo2List);
+        const spo2Data = new spo2List();
+        spo2Json.GetAll().forEach((element: any) => {
+            element = Object.assign(new spo2Unit(0, 0, false), element);
+            spo2Data.Add(element);
+        });
         return new GameData(
-            Object.assign(new bpmList(), data.bpmList),
-            Object.assign(new spo2List(), data.spo2List),
-            Object.assign(new Boolean(), data.didWin),
-            Object.assign(new Number(), data.finalScore),
-            Object.assign(new Number(), data.time),
-            Object.assign(new Date(), data.date),
-            Object.assign(new Number(), data.targetScore),
+            bpmData,
+            spo2Data,
+            data.didWin,
+            data.finalScore,
+            data.time,
+            new Date(data.date),
+            data.targetScore,
             data.fileName
         );
-    }
+    } 
 
     public static CreateFromInput(bpmList: bpmList, spo2List: spo2List, didWin: boolean, finalScore: number, time: number, date: Date, targetScore: number, fileName: string): GameData {
         return new GameData(bpmList, spo2List, didWin, finalScore, time, date, targetScore, fileName);
+    }
+
+    public static CreateTestData(): GameData {
+        const BpmList: bpmList = new bpmList();
+        BpmList.Add(new bpmUnit(1, 100, false));
+        BpmList.Add(new bpmUnit(2, 110, false));
+        BpmList.Add(new bpmUnit(3, 120, false));
+        const Spo2List: spo2List = new spo2List();
+        Spo2List.Add(new spo2Unit(1, 96, false));
+        Spo2List.Add(new spo2Unit(2, 96, false));
+        Spo2List.Add(new spo2Unit(3, 97, false));
+        const date = new Date();
+
+        return new GameData(
+            BpmList,
+            Spo2List,
+            false,
+            30,
+            10,
+            date,
+            100,
+            `gamedata_${date.toISOString()}`
+        )
     }
 
 }
