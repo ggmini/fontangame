4ef5a2a475745d0c5ca8480894ce37cc40af88b3
munrogr84298 <munrogr84298@th-nuernberg.de>
2025-08-28 11:45:48 +0200

    Added Angular Material Package

--- fontangame/src/app/app.config.ts
+++ fontangame/src/app/app.config.ts
@@ -5,10 +5,11 @@ import { routes } from './app.routes';
 import { MqttModule } from 'ngx-mqtt';
 
 import { MQTT_SERVICE_OPTIONS } from './credentials';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
-    importProvidersFrom(MqttModule.forRoot(MQTT_SERVICE_OPTIONS))
+    importProvidersFrom(MqttModule.forRoot(MQTT_SERVICE_OPTIONS)), provideAnimationsAsync()
   ]
 };
